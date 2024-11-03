2b1190ccfc448256087962418703e2fe9dd748fd
Matheus Muniz <matheusmuniz0810@gmail.com>
2024-11-03 17:22:02 -0300

    + Model da estação

--- appGrafos/src/model/Estacao.java
+++ appGrafos/src/model/Estacao.java
@@ -1,4 +1,38 @@
 package model;
 
 public class Estacao {
+    private String nome;
+    private Linha linha;
+
+    public Estacao() {
+    }
+
+    public Estacao(String nome, Linha linha) {
+        this.nome = nome;
+        this.linha = linha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Linha getLinha() {
+        return linha;
+    }
+
+    public void setLinha(Linha linha) {
+        this.linha = linha;
+    }
+
+    @Override
+    public String toString() {
+        return "Estacao{" +
+                "nome='" + nome + '\'' +
+                ", linha=" + linha +
+                '}';
+    }
 }
