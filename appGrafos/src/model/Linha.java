e243814686ae45ea4178c1ab5d6640e19b08b12f
Matheus Muniz <matheusmuniz0810@gmail.com>
2024-11-03 17:22:50 -0300

    + Model da classe Linha

--- appGrafos/src/model/Linha.java
+++ appGrafos/src/model/Linha.java
@@ -1,4 +1,40 @@
 package model;
 
+import java.util.List;
+
 public class Linha {
+    private int numeroLinha;
+    private List<Estacao> estacoes;
+
+    public Linha() {
+    }
+
+    public Linha(int numeroLinha, List<Estacao> estacoes) {
+        this.numeroLinha = numeroLinha;
+        this.estacoes = estacoes;
+    }
+
+    public int getNumeroLinha() {
+        return numeroLinha;
+    }
+
+    public void setNumeroLinha(int numeroLinha) {
+        this.numeroLinha = numeroLinha;
+    }
+
+    public List<Estacao> getEstacoes() {
+        return estacoes;
+    }
+
+    public void setEstacoes(List<Estacao> estacoes) {
+        this.estacoes = estacoes;
+    }
+
+    @Override
+    public String toString() {
+        return "Linha{" +
+                "numeroLinha=" + numeroLinha +
+                ", estacoes=" + estacoes +
+                '}';
+    }
 }
